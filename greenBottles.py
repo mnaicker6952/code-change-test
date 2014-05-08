76a4329c97bdd7aa13337b381c0defc72a30124e
mnaicker6952 <mnaicker6952@sohamcollege.co.uk>
2014-05-08 11:56:28 +0100

    Update greenBottles.py
    I added a loop and made it more efficient, and changed the 'hanging' to 'sitting' because it was wrong :).

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,7 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+#green bottles song thing############
+bottle=10
+for x in range (bottle,0,-1):
+    print(" {0} green bottles, sitting on the wall".format(x))
+    print("{0} green bottles, sitting on the wall".format(x))
+    print("And if 1 green bottle should acidentally fall,")
+    print("They'll be {0} green bottles sitting on the wall".format(x-1))
